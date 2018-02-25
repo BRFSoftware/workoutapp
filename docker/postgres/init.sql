491ab7ed8a59266cf24a1f953667206826344eb3
Nicolas Agostini <Nico@Colonys-MacBook-Pro.local>
2018-02-25 15:56:50 -0800

    Changes in the init.sql to be more correct

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,3 +1,10 @@
+CREATE role admin nologin;
+GRANT admin TO postgres;
+
+CREATE role anon nologin;
+GRANT anon TO postgres;
+GRANT USAGE ON SCHEMA public TO anon;
+
 CREATE TABLE Users(
    Id   SERIAL PRIMARY KEY,
    FirstName    VARCHAR(50) NOT NUlL,
@@ -18,22 +25,19 @@ CREATE TABLE train (
 	trainname varchar(254) NOT NULL,
 	traintype varchar(254) NOT NULL,
 	trainexercisetype varchar(254) NOT NULL,
-	trainrepetitions int4 NOT NULL,
+	trainrepetitiONs int4 NOT NULL,
 	trainequipmentid int4 NOT NULL,
 	trainuserid int4 NOT NULL
-)
-create role admin nologin;
-grant admin to postgres;
+);
 
-grant usage on schema public to admin;
-grant all on public.users to admin;
-grant usage, select on sequence public.users_id_seq to admin;
+GRANT USAGE ON SCHEMA public TO admin;
+GRANT ALL ON public.users TO admin;
+GRANT ALL ON public.equipment TO admin;
+GRANT ALL ON public.exercise TO admin;
+GRANT ALL ON public.train TO admin;
+GRANT USAGE, SELECT ON  sequence public.users_id_seq TO admin;
 
-create role anon nologin;
-grant anon to postgres;
-grant usage on schema public to anon;
-
-grant select on public.users to anon;
-grant select on public.equipment to anon;
-grant select on public.exercise to anon;
-grant select on public.train to anon;
+GRANT SELECT ON public.users TO anon;
+GRANT SELECT ON public.equipment TO anon;
+GRANT SELECT ON public.exercise TO anon;
+GRANT SELECT ON public.train TO anon;
