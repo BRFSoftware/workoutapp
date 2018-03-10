b89a52f47aaf35d2108e6d5fe9183b48107b27e2
Samuel Baruffi <samu.lord@gmail.com>
2018-03-10 12:26:45 -0800

    New anon rule

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -2,8 +2,8 @@ CREATE role admin nologin;
 GRANT admin TO postgres;
 
 CREATE role anon nologin;
-GRANT anon TO postgres;
-GRANT USAGE ON SCHEMA public TO anon;
+
+CREATE ROLE "rw-equipment" LOGIN;
 
 CREATE TABLE Users(
    Id   SERIAL PRIMARY KEY,
@@ -11,23 +11,23 @@ CREATE TABLE Users(
    LastName     VARCHAR(50) NOT NUlL
 );
 CREATE TABLE equipment (
-	equipmentId SERIAL PRIMARY KEY NOT NULL,
-	equipmentName VARCHAR(254) NOT NULL,
-	equipmentType VARCHAR(254) NOT NULL
+    equipmentId SERIAL PRIMARY KEY NOT NULL,
+    equipmentName VARCHAR(254) NOT NULL,
+    equipmentType VARCHAR(254) NOT NULL
 );
 CREATE TABLE exercise (
-	exerciseId SERIAL PRIMARY KEY NOT NULL,
-	exerciseName VARCHAR(254) NOT NULL,
-	exerciseType VARCHAR(254) NOT NULL
+    exerciseId SERIAL PRIMARY KEY NOT NULL,
+    exerciseName VARCHAR(254) NOT NULL,
+    exerciseType VARCHAR(254) NOT NULL
 );
 CREATE TABLE train (
-	trainid SERIAL PRIMARY KEY NOT NULL ,
-	trainname varchar(254) NOT NULL,
-	traintype varchar(254) NOT NULL,
-	trainexercisetype varchar(254) NOT NULL,
-	trainrepetitiONs int4 NOT NULL,
-	trainequipmentid int4 NOT NULL,
-	trainuserid int4 NOT NULL
+    trainid SERIAL PRIMARY KEY NOT NULL ,
+    trainname varchar(254) NOT NULL,
+    traintype varchar(254) NOT NULL,
+    trainexercisetype varchar(254) NOT NULL,
+    trainrepetitiONs int4 NOT NULL,
+    trainequipmentid int4 NOT NULL,
+    trainuserid int4 NOT NULL
 );
 
 GRANT USAGE ON SCHEMA public TO admin;
@@ -35,11 +35,7 @@ GRANT ALL ON public.users TO admin;
 GRANT ALL ON public.equipment TO admin;
 GRANT ALL ON public.exercise TO admin;
 GRANT ALL ON public.train TO admin;
-GRANT USAGE, SELECT ON  sequence public.users_id_seq TO admin;
+GRANT USAGE, SELECT ON sequence public.users_id_seq TO admin;
 
-
-GRANT SELECT ON public.users TO anon;
-GRANT SELECT ON public.equipment TO anon;
-GRANT SELECT ON public.exercise TO anon;
-GRANT SELECT ON public.train TO anon;
-
+GRANT Insert, Select ON TABLE "public"."equipment" TO "rw-equipment";
+GRANT Usage, Select ON TABLE "public"."equipment_equipmentid_seq" TO "rw-equipment";
