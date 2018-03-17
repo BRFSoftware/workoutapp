1375f19d194409746eeb38df1dce6bfd4842e598
Nicolas Agostini <Nico@Colonys-MacBook-Pro.local>
2018-03-17 14:30:38 -0700

    Fixing bootstrap

--- docker/webui/bottle/routes.py
+++ docker/webui/bottle/routes.py
@@ -25,6 +25,7 @@ def index():
     return template('index', data = data)
 
 @app.route('/equipment', method='GET')
+@app.route('equipment', method='GET')
 def equipment():
     token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoicnctZXF1aXBtZW50In0.yR78_tBuW1Da6wVG9rIvyzhlfbR1xtz7O5oXa1z6Xa4"
     headers = {
@@ -39,5 +40,22 @@ def equipment():
     print(data)
 
     return template('equipment', data=data)
+
+
+@app.route('/users', method='GET')
+@app.route('users', method='GET')
+def users():
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoicnctZXF1aXBtZW50In0.yR78_tBuW1Da6wVG9rIvyzhlfbR1xtz7O5oXa1z6Xa4"
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.get('http://api:3000/users', headers=headers)
+
+    data = json.loads(response.text)
+
+
+    return template('users', data=data)
 
 run(app, host='0.0.0.0', port = 8080)
